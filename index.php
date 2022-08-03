c53b67e6b0c112053d6c3191d8f8fa3c62cf597c
Frederick Ennin <kpin463@gmail.com>
2022-08-03 20:18:01 +0000

    dsfsd

--- index.php
+++ index.php
@@ -139,7 +139,7 @@
 	                              <input type="password" class="form-control" placeholder="Repeat password">
 	                            </div>
 	                            <div class="mb-3 text-center">
-	                              <button type="submit" class="btn btn-default btn-dark">Sign Me Up</button>
+	                              <button type="submit" class="btn btn-default btn-dark">Sign Up</button>
 	                            </div>
 	                          </form>
 	                        </div>

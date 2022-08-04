26d448d357361540f68b4c5771796dac144af2a9
Frederick Ennin <kolawolea844@gmail.com>
2022-08-04 19:05:33 +0000

    rreferf

--- includes/reciever.php
+++ includes/reciever.php
@@ -1,11 +1,12 @@
 <?php
 
-require '../loader/autoloader.php';
+require 'functions.php';
 
 if (isset($_GET['action'])) {
     switch ($_GET['action']) {
         case 'login':
-
+            extract($_POST);
+            login($email, $password);
             break;
 
         default:

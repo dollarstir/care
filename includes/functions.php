6658ff05cbe78e8dd1e9729a8b2a88f645c5657a
Frederick Ennin <kolawolea844@gmail.com>
2022-08-04 19:19:14 +0000

    erertet

--- includes/functions.php
+++ includes/functions.php
@@ -5,7 +5,13 @@
 function login($email, $password)
 {
     include 'dbcon.php';
-    $email  = mysqli_real_escape_string($conn,$email);
-    $password = mysqli_real_escape_string($conn,$password);
-    $sel = mysqli_query($conn,"SELECT * FROM staff WHERE ")
+    $email = mysqli_real_escape_string($conn, $email);
+    $password = md5(mysqli_real_escape_string($conn, $password));
+    $sel = mysqli_query($conn, "SELECT * FROM staff WHERE email = '$email' AND password ='$password'");
+    if (mysqli_num_rows($sel) >= 1) {
+        $row = mysqli_fetch_array($sel);
+        session_start();
+        $_SESSION['id'] = $row['id'];
+        echo 'loginsuccess';
+    }
 }

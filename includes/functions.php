db6928484a91e269537ded7dad8b34f116afbc2f
Frederick Ennin <kolawolea844@gmail.com>
2022-08-04 19:12:32 +0000

    ssdfdsf

--- includes/functions.php
+++ includes/functions.php
@@ -1,3 +1,11 @@
 <?php
 
-include 'dbcon.php';
+// Staff Login function
+
+function login($email, $password)
+{
+    include 'dbcon.php';
+    $email  = mysqli_real_escape_string($conn,$email);
+    $password = mysqli_real_escape_string($conn,$password);
+    $sel = mysqli_query($conn,"SELECT * FROM staff WHERE ")
+}

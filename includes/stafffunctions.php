2afb76ef3e25cc3f5a48258b41daa6f2b8bd797e
Frederick Ennin <kpin463@gmail.com>
2022-08-06 06:54:44 +0000

    sfsd

--- includes/stafffunctions.php
+++ includes/stafffunctions.php
@@ -51,7 +51,7 @@ function begin($title)
  {
      echo '<nav class="sidebar">
      <div class="logo d-flex justify-content-between">
-         <a href="index.html"><img src="img/logo.png" alt=""></a>
+         <a href="index.html"><img src="../assets/images/logo.png" alt=""></a>
          <div class="sidebar_close_icon d-lg-none">
              <i class="ti-close"></i>
          </div>

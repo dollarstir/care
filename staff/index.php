7a3dd962c9794dcb079e547084765c1601afecb2
Frederick Ennin <kpin463@gmail.com>
2022-08-06 06:32:19 +0000

    sdfdsfs

--- staff/index.php
+++ staff/index.php
@@ -83,68 +83,14 @@ begin('Home'); ?>
                                                     <p>Worked on</p>
                                                 </div>
                                             </div>
-                                            <div class="single_quick_activity d-flex">
-                                                <div class="icon">
-                                                    <img src="img/icon/wheel.svg" alt="">
-                                                </div>
-                                                <div class="count_content">
-                                                    <h3><span class="counter">7510</span> </h3>
-                                                    <p>Patients</p>
-                                                </div>
-                                            </div>
-                                            <div class="single_quick_activity d-flex">
-                                                <div class="icon">
-                                                    <img src="img/icon/pharma.svg" alt="">
-                                                </div>
-                                                <div class="count_content">
-                                                    <h3><span class="counter">2110</span> </h3>
-                                                    <p>Pharmacusts</p>
-                                                </div>
-                                            </div>
+                                            
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-12 col-xl-12">
-                        <div class="white_box mb_30 ">
-                            <div class="box_header border_bottom_1px  ">
-                                <div class="main-title">
-                                    <h3 class="mb_25">Hospital Survey</h3>
-                                </div>
-                            </div>
-                            <div class="income_servay">
-                                <div class="row">
-                                    <div class="col-md-3">
-                                        <div class="count_content">
-                                            <h3>$ <span class="counter">305</span> </h3>
-                                            <p>Today's Income</p>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-3">
-                                        <div class="count_content">
-                                            <h3>$ <span class="counter">1005</span> </h3>
-                                            <p>This Week's Income</p>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-3">
-                                        <div class="count_content">
-                                            <h3>$ <span class="counter">5505</span> </h3>
-                                            <p>This Month's Income</p>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-3">
-                                        <div class="count_content">
-                                            <h3>$ <span class="counter">155615</span> </h3>
-                                            <p>This Year's Income</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div id="bar_wev"></div>
-                        </div>
-                    </div>
+                    
                     <div class="col-xl-7">
                         <div class="white_box QA_section card_height_100">
                             <div class="white_box_tittle list_header m-0 align-items-center">

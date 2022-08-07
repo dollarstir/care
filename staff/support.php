03f94733f2d434068e1a91ffa71f2bfdbe552f7c
Frederick Ennin <kpin463@gmail.com>
2022-08-07 09:18:53 +0000

    dfsfds

---
+++ staff/support.php
@@ -0,0 +1,185 @@
+<?php
+include '../includes/stafffunctions.php';
+
+begin('Support'); ?>
+
+<body class="crm_body_bg">
+
+
+
+<?php staffsidebar(); ?>
+
+
+    <section class="main_content dashboard_part">
+
+    <?php staffheader(); ?>
+
+        <div class="main_content_iner ">
+            <div class="container-fluid p-0">
+                <div class="row justify-content-center">
+                    <div class="col-lg-12">
+                        <div class="white_box mb_30">
+                            <div class="box_header ">
+                                <div class="main-title">
+                                    <h3 class="mb-0">Input select 1</h3>
+                                </div>
+                            </div>
+                            <form>
+                                <div class="mb-3">
+                                    <label class="form-label" for="exampleFormControlInput1">Email address</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1"
+                                        placeholder="name@example.com">
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label" for="exampleFormControlSelect1">Example select</label>
+                                    <select class="form-control" id="exampleFormControlSelect1">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label" for="exampleFormControlSelect2">Example multiple
+                                        select</label>
+                                    <select multiple class="form-control" id="exampleFormControlSelect2">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="white_box mb_30">
+                            <div class="box_header ">
+                                <div class="main-title">
+                                    <h3 class="mb-0">Input select 2</h3>
+                                </div>
+                            </div>
+                            <select class="nice_Select mb_30">
+                                <option data-display="Select">Nothing</option>
+                                <option value="1">Some option</option>
+                                <option value="2">Another option</option>
+                                <option value="3" disabled>A disabled option</option>
+                                <option value="4">Potato</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="white_box mb_30">
+                            <div class="box_header ">
+                                <div class="main-title">
+                                    <h3 class="mb-0">Input select 3</h3>
+                                </div>
+                            </div>
+                            <select class="default_sel mb_30 w-100">
+                                <option data-display="Select">Nothing</option>
+                                <option value="1">Some option</option>
+                                <option value="2">Another option</option>
+                                <option value="3" disabled>A disabled option</option>
+                                <option value="4">Potato</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="white_box mb_30">
+                            <div class="box_header ">
+                                <div class="main-title">
+                                    <h3 class="mb-0">Input select 4</h3>
+                                </div>
+                            </div>
+                            <select class="default_sel mb_30">
+                                <option data-display="Select">Nothing</option>
+                                <option value="1">Some option</option>
+                                <option value="2">Another option</option>
+                                <option value="3" disabled>A disabled option</option>
+                                <option value="4">Potato</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="white_box mb_30">
+                            <div class="box_header ">
+                                <div class="main-title">
+                                    <h3 class="mb-0">Input select 5</h3>
+                                </div>
+                            </div>
+                            <select class="form-select form-control-lg mb_30">
+                                <option>Large select</option>
+                            </select>
+                            <select class="form-select mb_30">
+                                <option>Default select</option>
+                            </select>
+                            <select class="form-select form-control-sm">
+                                <option>Small select</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer_part">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="footer_iner text-center">
+                                <p>2020 © Influence - Designed by <a href="#"> <i class="ti-heart"></i> </a><a href="#">
+                                        Dashboard</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+
+
+
+    <script src="js/jquery1-3.4.1.min.js"></script>
+
+    <script src="js/popper1.min.js"></script>
+
+    <script src="js/bootstrap1.min.js"></script>
+
+    <script src="js/metisMenu.js"></script>
+
+    <script src="vendors/count_up/jquery.waypoints.min.js"></script>
+
+    <script src="vendors/chartlist/Chart.min.js"></script>
+
+    <script src="vendors/count_up/jquery.counterup.min.js"></script>
+
+    <script src="vendors/swiper_slider/js/swiper.min.js"></script>
+
+    <script src="vendors/niceselect/js/jquery.nice-select.min.js"></script>
+
+    <script src="vendors/owl_carousel/js/owl.carousel.min.js"></script>
+
+    <script src="vendors/gijgo/gijgo.min.js"></script>
+
+    <script src="vendors/datatable/js/jquery.dataTables.min.js"></script>
+    <script src="vendors/datatable/js/dataTables.responsive.min.js"></script>
+    <script src="vendors/datatable/js/dataTables.buttons.min.js"></script>
+    <script src="vendors/datatable/js/buttons.flash.min.js"></script>
+    <script src="vendors/datatable/js/jszip.min.js"></script>
+    <script src="vendors/datatable/js/pdfmake.min.js"></script>
+    <script src="vendors/datatable/js/vfs_fonts.js"></script>
+    <script src="vendors/datatable/js/buttons.html5.min.js"></script>
+    <script src="vendors/datatable/js/buttons.print.min.js"></script>
+    <script src="js/chart.min.js"></script>
+
+    <script src="vendors/progressbar/jquery.barfiller.js"></script>
+
+    <script src="vendors/tagsinput/tagsinput.js"></script>
+
+    <script src="vendors/text_editor/summernote-bs4.js"></script>
+    <script src="vendors/apex_chart/apexcharts.js"></script>
+
+    <script src="js/custom.js"></script>
+</body>
+
+</html>

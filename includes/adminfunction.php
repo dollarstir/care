4bfede61309428a5e386a9dd9d38d65a0c039eba
Frederick Ennin <kpin463@gmail.com>
2022-08-10 12:36:04 +0000

    sfewfwf

--- includes/adminfunction.php
+++ includes/adminfunction.php
@@ -202,3 +202,59 @@ function undertreatment()
 
     return $count;
 }
+
+function addstaff($name, $email, $phone, $dob, $password, $repass)
+{
+    include 'dbcon.php';
+    // making sure all fields are filled
+    if (empty(trim($name)) || empty(trim($email)) || empty(trim($phone)) || empty(trim($dob)) || empty(trim($password)) || empty(trim($repass))) {
+        echo 'please fill all fields';
+    } else {
+        if ($repass != $password) {
+            echo 'Password mismatch';
+        } elseif (empty($_FILES['image']['name'])) {
+            echo 'kindly aploud your profile picture';
+        } else {
+            // checking if user exist
+            $check = mysqli_query($conn, "SELECT * FROM staff WHERE email ='$email' OR phone ='$phone'");
+            if (mysqli_num_rows($check) >= 1) {
+                echo 'User already exist';
+            } else {
+                // inserting the user data into the database
+                // $image = $_FILES['image']['name'];
+                $image_name = $_FILES['image']['name'];
+                $image_tmp = $_FILES['image']['tmp_name'];
+                $image_size = $_FILES['image']['size'];
+                $image_error = $_FILES['image']['error'];
+                $image_type = $_FILES['image']['type'];
+                $image_ext = explode('.', $image_name);
+                $image_ext = strtolower(end($image_ext));
+                $allowed = ['jpg', 'jpeg', 'png'];
+                if (in_array($image_ext, $allowed)) {
+                    if ($image_error === 0) {
+                        if ($image_size <= 2097152) {
+                            $image_name_new = uniqid('', true).'.'.$image_ext;
+                            $image_destination = '../upload/'.$image_name_new;
+                            move_uploaded_file($image_tmp, $image_destination);
+                            $password = md5(mysqli_real_escape_string($conn, $password));
+                            $datejoined = date('jS F, Y');
+                            $name = $fname.' '.$lname;
+                            $insert = mysqli_query($conn, "INSERT INTO staff (name,email,phone,dob,pic,password,datejoined) VALUES ('$name','$email','$phone','$dob','$image_name_new','$password','$datejoined')");
+                            if ($insert) {
+                                echo 'Registration successful';
+                            } else {
+                                echo 'Registration failed';
+                            }
+                        } else {
+                            echo 'Image size is too big';
+                        }
+                    } else {
+                        echo 'There was an error uploading your image';
+                    }
+                } else {
+                    echo 'Image type is not allowed';
+                }
+            }
+        }
+    }
+}

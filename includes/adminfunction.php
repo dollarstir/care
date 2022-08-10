4fa4561d4b0598ab1e89414650f4ab86d5e5e699
Frederick Ennin <kpin463@gmail.com>
2022-08-10 13:20:33 +0000

    saddas

--- includes/adminfunction.php
+++ includes/adminfunction.php
@@ -277,8 +277,9 @@ function liststaff()
                 <td>'.$row['email'].'</td>
                 <td>'.$row['phone'].'</td>
                 <td>'.$row['dob'].'</td>
+                <td>'.$row['datejoined'].'</td>
+                
                 <td>
-                    '.$row['datejoined'].'
                     <div class="amoutn_action d-flex align-items-center">
                                                         
                         <div class="dropdown ms-4">
@@ -293,12 +294,8 @@ function liststaff()
                             </div>
                         </div>
                     </div>
-                
-                
-                
+
                 </td>
-                
-                
                 
                 
             </tr>';

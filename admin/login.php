56d429fd02d303be8f6d44f052204d47c001329c
Frederick Ennin <kpin463@gmail.com>
2022-08-10 08:41:51 +0000

    sdfds

--- admin/login.php
+++ admin/login.php
@@ -1,7 +1,52 @@
 <?php
 include '../includes/adminfunction.php';
 
-begin('Admin Login'); ?>
+ ?>
+
+<!DOCTYPE html>
+    <html lang="zxx">
+    
+    <head>
+    
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <title>Login</title>
+        <link rel="icon" href="img/logo.png" type="image/png">
+    
+        <link rel="stylesheet" href="css/bootstrap1.min.css" />
+    
+        <link rel="stylesheet" href="vendors/themefy_icon/themify-icons.css" />
+    
+        <link rel="stylesheet" href="vendors/swiper_slider/css/swiper.min.css" />
+    
+        <link rel="stylesheet" href="vendors/select2/css/select2.min.css" />
+    
+        <link rel="stylesheet" href="vendors/niceselect/css/nice-select.css" />
+    
+        <link rel="stylesheet" href="vendors/owl_carousel/css/owl.carousel.css" />
+    
+        <link rel="stylesheet" href="vendors/gijgo/gijgo.min.css" />
+    
+        <link rel="stylesheet" href="vendors/font_awesome/css/all.min.css" />
+        <link rel="stylesheet" href="vendors/tagsinput/tagsinput.css" />
+    
+        <link rel="stylesheet" href="vendors/datatable/css/jquery.dataTables.min.css" />
+        <link rel="stylesheet" href="vendors/datatable/css/responsive.dataTables.min.css" />
+        <link rel="stylesheet" href="vendors/datatable/css/buttons.dataTables.min.css" />
+    
+        <link rel="stylesheet" href="vendors/text_editor/summernote-bs4.css" />
+    
+        <link rel="stylesheet" href="vendors/morris/morris.css">
+    
+        <link rel="stylesheet" href="vendors/material_icon/material-icons.css" />
+    
+        <link rel="stylesheet" href="css/metisMenu.css">
+
+        <link rel="stylesheet" href="../assets/css/sweetalert2.min.css">
+    
+        <link rel="stylesheet" href="css/style1.css" />
+        <link rel="stylesheet" href="css/colors/default.css" id="colorSkinCSS">
+    </head>
 
 <body class="crm_body_bg">
 
